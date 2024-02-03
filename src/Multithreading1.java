977122bfb187780cf2fa952d355d46c33204e21f
Chandan Kumar <85821143+ChandanArya20@users.noreply.github.com>
2024-02-03 14:21:52 +0530

    Add files via upload

--- src/Multithreading1.java
+++ src/Multithreading1.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.time.LocalTime;
 
 public class Multithreading1 {
@@ -29,3 +30,36 @@ public class Multithreading1 {
         System.out.println("Time taken "+ (System.currentTimeMillis()-startTime));
     }
 }
+=======
+import java.time.LocalTime;
+
+public class Multithreading1 {
+
+    public static void main(String[] args) {
+
+        long startTime = System.currentTimeMillis();
+
+
+        for (int i = 0; i < 10;i++){
+            System.out.print("-");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        for (int i = 0; i < 10;i++){
+            System.out.print("*");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        System.out.println();
+        System.out.println("Time taken "+ (System.currentTimeMillis()-startTime));
+    }
+}
+>>>>>>> origin/master
